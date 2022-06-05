29f781434f52c0498b65cc4faa0537ffb33b2aad
Alperen DGRYL <64389723+Alperendgryl@users.noreply.github.com>
2022-06-05 17:14:49 +0300

    Add files via upload

---
+++ Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSpeak = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnListen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnSpeak
+            // 
+            this.BtnSpeak.Location = new System.Drawing.Point(0, 0);
+            this.BtnSpeak.Name = "BtnSpeak";
+            this.BtnSpeak.Size = new System.Drawing.Size(211, 62);
+            this.BtnSpeak.TabIndex = 0;
+            this.BtnSpeak.Text = "Speak";
+            this.BtnSpeak.UseVisualStyleBackColor = true;
+            this.BtnSpeak.Click += new System.EventHandler(this.BtnSpeak_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(426, 109);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnListen
+            // 
+            this.BtnListen.Location = new System.Drawing.Point(217, 0);
+            this.BtnListen.Name = "BtnListen";
+            this.BtnListen.Size = new System.Drawing.Size(211, 62);
+            this.BtnListen.TabIndex = 2;
+            this.BtnListen.Text = "Listen";
+            this.BtnListen.UseVisualStyleBackColor = true;
+            this.BtnListen.Click += new System.EventHandler(this.BtnListen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 183);
+            this.Controls.Add(this.BtnListen);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnSpeak);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSpeak;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnListen;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpeechLib;
+using System.Speech.Recognition;
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSpeak_Click(object sender, EventArgs e)
+        {
+            
+
+
+            SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+            Grammar g = new DictationGrammar();
+            sr.LoadGrammar(g);
+            
+
+            try
+            {
+                BtnSpeak.Text = "Please Speak";
+                sr.SetInputToDefaultAudioDevice();
+                RecognitionResult res = sr.Recognize();
+                richTextBox1.Text = res.Text;
+
+
+
+            }
+            catch (NullReferenceException)
+            {
+                BtnSpeak.Text = "ERROR";
+
+            }
+
+
+        }
+
+        private void BtnListen_Click(object sender, EventArgs e)
+        {
+            SpVoice ses = new SpVoice();
+            ses.Speak(richTextBox1.Text);
+        }
+    }
+}
